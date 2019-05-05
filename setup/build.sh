242107cc6eec2053e9a3ac227029384917c36403
Chris Duck <gpduck@whatsupduck.net>
2019-05-05 05:06:20 -0500

    Install pwsh as separate file

--- setup/build.sh
+++ setup/build.sh
@@ -27,18 +27,6 @@ pushd ~
   popd
 popd
 
-# Install PowerShell
-PSFILE=powershell-6.2.0-linux-arm32.tar.gz
-sudo apt-get install libunwind8
-pushd ~
-  wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/$PSFILE
-  sudo mkdir -p /opt/microsoft/powershell/6
-  sudo tar -xvf ./$PSFILE -C /opt/microsoft/powershell/6
-  sudo chmod +x /opt/microsoft/powershell/6/pwsh
-  sudo ln -s /opt/microsoft/powershell/6/pwsh /usr/bin/pwsh
-  rm $PSFILE
-popd
-
 # Remove extra packages
 sudo apt remove -y \
     chromium-browser \
